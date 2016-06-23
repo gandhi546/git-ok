9ef885e42123b0c424d9c8f06411cfa07fe260ea
gandhi <ingandhi4@gmail.com>
2016-06-23 20:06:59 +0900

    12

--- study_okjsp2015/WebContent/WEB-INF/sql/okboard_info.sql
+++
@@ -1,8 +0,0 @@
-select * from okboard_info;
-
-insert into okboard_info (cseq, bbsid, name, header) 
-values (1, 'guideline', '프리랜서 가이드라인', '프리랜서 가이드라인 연재 게시판입니다.');
-
-select * from okboard_info where bbsid = 'guideline';
-
-update okboard_info set header = '전자정부 표준프레임워크 관련 게시판입니다.' where bbsid = 'egov';
